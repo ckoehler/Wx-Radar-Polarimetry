ea7bf2179b3b705eca2e75fa2fb1bc33f8d25de5
Christoph <christoph@zerodeviation.net>
2010-08-29 14:24:49 -0500

    finish problem 1

--- hw1/hw1.m
+++ hw1/hw1.m
@@ -1,4 +1,11 @@
 clear all; 
+
+% toggle problem 1 stuff
+p1 = false;
+
+
+
+
 close all;
 load dsddata_20050513.mat
 len=length(dsd_data(1,1,:));
@@ -14,13 +21,13 @@ D=D_all(:,1);
 Nd=Nd_all(:,1);
 
 % Surface distribution A(D)
-Ad = pi.*D_all.^2 .* Nd_all;
+Ad_all = pi.*D_all.^2 .* Nd_all;
 
 % Mass distribution M(D)
-Md = pi/6 * D_all.^3 .* Nd_all;
+Md_all = pi/6 * D_all.^3 .* Nd_all;
 
 % Reflectivity distribution Z(D)
-Zd = D_all.^6 .* Nd_all;
+Zd_all = D_all.^6 .* Nd_all;
 
 % create the time matrix. 481 data points between
 % 7 and 12 o'clock
@@ -30,8 +37,38 @@ for k=1:41
   t(k,:) = t1;
 end
 
-% now plot the whole thing
-pcolor([t1], [D_all], [Ct_all]);
-colormap(jet);
-colorbar;
+% only plot if enabled at the top
+if (p1 == true)
+  % now plot the whole thing
+  subplot(4,1,1);
+  pcolor([t1], [D_all], [log10(Nd_all)]);
+  shading interp;
+  colorbar;
+  xlabel('Time, UTC');
+  ylabel('D,mm');
+  title('N(D), log_{10} (m^{-3} mm^{-1})');
 
+  subplot(4,1,2);
+  pcolor([t1], [D_all], [log10(Ad_all)]);
+  shading interp;
+  colorbar;
+  xlabel('Time, UTC');
+  ylabel('D,mm');
+  title('A(D), log_{10} (mm^2 m^{-3} mm^{-1})');
+
+  subplot(4,1,3);
+  pcolor([t1], [D_all], [log10(Md_all)]);
+  shading interp;
+  colorbar;
+  xlabel('Time, UTC');
+  ylabel('D,mm');
+  title('M(D), log_{10} (g m^{-3} mm^{-1})');
+
+  subplot(4,1,4);
+  pcolor([t1], [D_all], [log10(Zd_all)]);
+  shading interp;
+  colorbar;
+  xlabel('Time, UTC');
+  ylabel('D,mm');
+  title('Z(D), log_{10} (mm^6 m^{-3} mm^{-1})');
+end
